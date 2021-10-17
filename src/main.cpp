70d5fee3ffae8364a43a646f631224db17c315dc
ReBeLTheProgrammer <darkwarrior007@ya.ru>
2021-10-17 21:57:57 +0300

    Add command parameter support

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <Server.h>
+#include <boost/program_options.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    std::uint16_t port = -1;
+    unsigned threadCount = -1;
+
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "print this help message")
+            ("threads", boost::program_options::value<unsigned>(&threadCount)->default_value(std::thread::hardware_concurrency()), "set the maximum cores to be used")
+            ("port", boost::program_options::value<std::uint16_t>(&port), "set the port for the control connections");
+
+    boost::program_options::variables_map options;
+
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), options);
+    boost::program_options::notify(options);
+
+    if(options.count("help")){
+        std::cout << desc << '\n';
+        return 1;
+    }
+
+    std::cout << "port: " << port << "\nthreads: " << threadCount << '\n';
+
     signal(SIGPIPE, SIG_IGN);
     sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_port = 25554;
+    address.sin_port = htons(port);
     address.sin_addr.s_addr = inet_addr("192.168.178.36");
-    auto controller = ftp::Server(address, std::filesystem::current_path(), 8);
+    static auto controller = ftp::Server(address, std::filesystem::current_path(), threadCount);
+
     try {
         controller.start();
     } catch(std::exception& e){
